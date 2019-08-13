19eef3cc877d5f5d6592baff5067bce2942647ff
445370510 <445370510@qq.com>
2019-08-13 18:39:04 +0800

    bucket tagging

---
+++ test/src/Bucket/BucketTaggingtTest.cpp
@@ -0,0 +1,163 @@
+/*
+ * Copyright 2009-2017 Alibaba Cloud All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <alibabacloud/oss/OssClient.h>
+#include "../Config.h"
+#include "../Utils.h"
+
+namespace AlibabaCloud 
+{
+namespace OSS 
+{
+
+    class BucketTaggingTest : public ::testing::Test {
+    protected:
+        BucketTaggingTest()
+        {
+        }
+
+        ~BucketTaggingTest() override
+        {
+        }
+
+        // Sets up the stuff shared by all tests in this test case.
+        static void SetUpTestCase()
+        {
+            Client = std::make_shared<OssClient>(Config::Endpoint, Config::AccessKeyId, Config::AccessKeySecret, ClientConfiguration());
+            BucketName = TestUtils::GetBucketName("cpp-sdk-buckettagging");
+            Client->CreateBucket(CreateBucketRequest(BucketName));
+        }
+
+        // Tears down the stuff shared by all tests in this test case.
+        static void TearDownTestCase()
+        {
+            Client->DeleteBucket(DeleteBucketRequest(BucketName));
+            Client = nullptr;
+        }
+
+        // Sets up the test fixture.
+        void SetUp() override
+        {
+        }
+
+        // Tears down the test fixture.
+        void TearDown() override
+        {
+        }
+    public:
+        static std::shared_ptr<OssClient> Client;
+        static std::string BucketName;
+    };
+
+    std::shared_ptr<OssClient> BucketTaggingTest::Client = nullptr;
+    std::string BucketTaggingTest::BucketName = "";
+
+    TEST_F(BucketTaggingTest, SetAndDeleteBucketTaggingTest)
+    {
+        SetBucketTaggingRequest setrequest(BucketName);
+        Tag tag1("project","projectone");
+        Tag tag2("user", "jsmith");
+        TagSet tagset;
+        tagset.push_back(tag1);
+        tagset.push_back(tag2);
+        Tagging taging;
+        taging.setTags(tagset);
+        setrequest.setTagging(taging);
+        auto setoutcome = Client->SetBucketTagging(setrequest);
+        EXPECT_EQ(setoutcome.isSuccess(), true);
+
+        DeleteBucketTaggingRequest delrequest(BucketName);
+        auto deloutcome = Client->DeleteBucketTagging(delrequest);
+        EXPECT_EQ(deloutcome.isSuccess(), true);
+    }
+
+    TEST_F(BucketTaggingTest, GetBucketTaggingTest)
+    {
+        SetBucketTaggingRequest setrequest(BucketName);
+        Tag tag1("project", "projectone");
+        Tag tag2("user", "jsmith");
+        TagSet tagset;
+        tagset.push_back(tag1);
+        tagset.push_back(tag2);
+        Tagging taging;
+        taging.setTags(tagset);
+        setrequest.setTagging(taging);
+        auto setoutcome = Client->SetBucketTagging(setrequest);
+        EXPECT_EQ(setoutcome.isSuccess(), true);
+
+        auto getoutcome = Client->GetBucketTagging(GetBucketTaggingRequest(BucketName));
+        EXPECT_EQ(getoutcome.isSuccess(), true);
+        EXPECT_EQ(getoutcome.result().Tagging().Tags().size(), 2U);
+        EXPECT_TRUE(getoutcome.result().RequestId().size() > 0U);
+
+        size_t i = 0;
+        for (const auto& tag : getoutcome.result().Tagging().Tags()) {
+            EXPECT_EQ(taging.Tags()[i].Key(), tag.Key());
+            EXPECT_EQ(taging.Tags()[i].Value(), tag.Value());
+            i++;
+        }
+
+        DeleteBucketTaggingRequest delrequest(BucketName);
+        auto deloutcome = Client->DeleteBucketTagging(delrequest);
+        EXPECT_EQ(deloutcome.isSuccess(), true);
+    }
+
+    TEST_F(BucketTaggingTest, GetBucketTaggingResult)
+    {
+        std::string xml = R"(<?xml version="1.0" ?>
+                        <Tagging>
+                            <TagSet>
+                            <Tag>
+                                <Key>project</Key>
+                                <Value>projectone</Value>
+                            </Tag>
+                            <Tag>
+                                <Key>user</Key>
+                                <Value>jsmith</Value>
+                            </Tag>
+                            </TagSet>
+                        </Tagging>)";
+        GetBucketTaggingResult result1(xml);
+        EXPECT_EQ(result1.Tagging().Tags().size(), 2U);
+        EXPECT_EQ(result1.Tagging().Tags()[0].Key(), "project");
+        EXPECT_EQ(result1.Tagging().Tags()[0].Value(), "projectone");
+        EXPECT_EQ(result1.Tagging().Tags()[1].Key(), "user");
+        EXPECT_EQ(result1.Tagging().Tags()[1].Value(), "jsmith");
+    }
+
+    TEST_F(BucketTaggingTest, ListBucketByTaggingTest)
+    {
+        SetBucketTaggingRequest setrequest(BucketName);
+        Tag tag1("project", "projectone");
+        TagSet tagset;
+        tagset.push_back(tag1);
+        Tagging taging;
+        taging.setTags(tagset);
+        setrequest.setTagging(taging);
+        auto setoutcome = Client->SetBucketTagging(setrequest);
+        EXPECT_EQ(setoutcome.isSuccess(), true);
+
+        ListBucketsRequest listrequest;
+        listrequest.setTag(tag1);
+        auto listoutcome = Client->ListBuckets(listrequest);
+
+        EXPECT_EQ(listoutcome.isSuccess(), true);
+        EXPECT_EQ(listoutcome.result().Buckets().size(), 1U);
+        EXPECT_EQ(listoutcome.result().Buckets()[0].Name(), BucketName);
+    }
+}
+}
